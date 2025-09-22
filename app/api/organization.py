105935239494bd9be8d7a47203d91eb0abf83e0b
k <wetarmor@gmail.com>
2025-09-22 22:40:58 +0300

    Added endpoint to get all organizations by activity id

--- app/api/organization.py
+++ app/api/organization.py
@@ -15,6 +15,7 @@ from app.schemas.organization import (
     OrganizationWithPhones,
 )
 from app.services.organization import (
+    get_organizations_by_activity,
     get_organizations_by_building,
 )
 
@@ -29,9 +30,26 @@ router = APIRouter(
 @router.get('/by-building', response_model=list[OrganizationWithPhones])
 async def organization_list_by_building(building_id: int, session: SessionDep):
     """
-    Endpoint to get all organization in building.
+    Endpoint to get all organizations located in the specified building.
     """
     organizations = await get_organizations_by_building(building_id, session)
+
+    return [
+        {
+            "id": org.id,
+            "name": org.name,
+            "phones": [phone.phone for phone in org.phones],
+        }
+        for org in organizations
+    ]
+
+
+@router.get('/by-activity', response_model=list[OrganizationWithPhones])
+async def organization_list_by_activity(activity_id: int, session: SessionDep):
+    """
+    Endpoint to get all organizations associated with a certain activity.
+    """
+    organizations = await get_organizations_by_activity(activity_id, session)
 
     return [
         {
--- app/services/organization.py
+++ app/services/organization.py
@@ -1,14 +1,19 @@
 from sqlalchemy import (
     select,
 )
-from sqlalchemy.orm import selectinload
+from sqlalchemy.orm import (
+    selectinload,
+)
 
+from app.models.activity import (
+    Activity,
+)
 from app.models.organization import (
     Organization,
 )
 
 
-async def get_organizations_by_building(building_id, session):
+async def get_organizations_by_building(building_id: int, session):
     """
     Returns all organization related to building.
     """
@@ -20,3 +25,19 @@ async def get_organizations_by_building(building_id, session):
     result = await session.execute(query)
 
     return result.scalars().unique().all()
+
+
+async def get_organizations_by_activity(activity_id: int, session):
+    """
+    Returns all organizations associated with a certain activity.
+    """
+    query = select(Organization).join(
+        Organization.activities,
+    ).where(
+        Activity.id == activity_id,
+    ).options(
+        selectinload(Organization.phones),
+    )
+    result = await session.execute(query)
+
+    return result.scalars().all()
